8ffdb47980e7a0a0990e0a5349640a31cc5437fd
Jim Was Here <43002597+r3dxpl0it@users.noreply.github.com>
2018-11-12 15:55:23 +0100

    Update dos.py

--- dos.py
+++ dos.py
@@ -1,11 +1,20 @@
-"""
+'''
+_____________________________________________________________________
+|[] R3DXPL0IT SHELL                                            |ROOT]|!"|
+|"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""|"| 
+|CODED BY > R3DXPLOIT(JIMMY)                                          | |
+|EMAIL > RETURN_ROOT@PROTONMAIL.COM                                   | |
+|GITHUB > https://github.com/r3dxpl0it                                | |
+|WEB-PAGE > https://r3dxpl0it.Github.io                               |_|
+|_____________________________________________________________________|/|
+
 Test node responses to invalid blocks.
 In this test we connect to one node over p2p, and test block requests:
 1) Valid blocks should be requested and become chain tip.
 2) Invalid block with duplicated transaction should be re-requested.
 3) Invalid block with bad coinbase value should be rejected and not
 re-requested.
-"""
+'''
 import copy
 import struct
 
